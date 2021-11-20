08095c0667a1c33d031ff0f04fcc4fff84ef6f0a
Fukin Ivan <fukinivan870@gmail.com>
2021-11-20 23:25:25 +0300

    Hello

---
+++ Test.py
@@ -0,0 +1,19 @@
+print(int('101', 2))
+x = complex(3, 4)
+y = complex(1, 2)
+import math
+import random
+z = '{} Nikolay '
+v = 'Pustov'
+s = '%s %s' %(z,v)
+j = '{1} {1}' .format(z,v)
+i = f'{z} {v}'
+c=r'C:\newt.txt' 
+print(math.sqrt(9))
+print(random.randint(1,9))
+print(abs(x))
+print(z)
+print(c)
+print(s)
+print(j)
+print(v[11:0:-1])
